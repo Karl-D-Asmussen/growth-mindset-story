6a2600b473395e906eebad73baacbe30005285da
Karl D. Asmussen <karl.asmussen@cpr.ku.dk>
2018-04-03 23:14:21 +0200

    Tue Apr  3 23:14:21 CEST 2018

--- tool.rb
+++ tool.rb
@@ -3,7 +3,7 @@
 require 'yaml'
 
 filenames = Dir['*.mkdn'].grep(/^\d+\.mkdn$/).sort_by { |s| /\d+/ =~ s; $&.to_i }
-things = {}
+things = Hash.new { |h,k| h[k] = {} }
 
 filenames.each do |filename|
   File.open filename do |file|
@@ -11,15 +11,30 @@ filenames.each do |filename|
       if /<!--(.+)::(.+)=(\s*\d+\.\d\d\s*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        (things[ns] ||= {})[it] ||= "0.00"
-        things[ns][it] = "%.02f" % (things[ns][it].to_r + $3.strip.to_r)
-      elsif /<!--(.+)::(.+)\|(.+)-->/ =~ line
+        things[ns][it] = $3.strip
+      if /<!--(.+)::(.+)\+=(\s*\d+\.\d\d\s*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        (things[ns] ||= {})[it] ||= []
+        things[ns][it] ||= '0.00'
+        things[ns][it] = '%.02f' % (things[ns][it].to_r + $3.strip.to_r)
+      elsif /<!--(.+)::(.+)@(.*)-->/ =~ line
+        ns = $1.split.join
+        it = $2.split.join
+        things[ns][it] = ($3.empty? ? [] : [$3.strip])
+      elsif /<!--(.+)::(.+)\+@(.*)-->/ =~ line
+        ns = $1.split.join
+        it = $2.split.join
+        things[ns][it] ||= []
         things[ns][it] << $3.strip
+      elsif /<!--(.+)::(.+)!\s*-->/ =~ line
+        ns = $1.split.join
+        it = $2.split.join
+        things[ns].delete(it)
+        if things[ns].empty?
+          things.delete(ns)
+        end
       elsif /<!--.*-->/ =~ line
-        $stderr.puts "??? #$&"
+        $stderr.puts "error #$&"
       end
     end
   end
