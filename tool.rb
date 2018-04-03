582ad90c9a1134e2f575512ad5ca504f5b48b7e8
Karl D. Asmussen <karl.asmussen@cpr.ku.dk>
2018-04-03 12:12:09 +0200

    Tue Apr  3 12:12:09 CEST 2018

---
+++ tool.rb
@@ -0,0 +1,22 @@
+
+require 'yaml'
+
+filenames = Dir['./*.mkdn'].grep(/^\d+.mknd$/).sort_by { |s| /\d+/ =~ s; $&.to_i }
+things = {}
+
+filenames.each do |filename|
+  File.open filename do |file|
+    file.each_line do |line|
+      if /<!--(.+)::(.+)\|(.+)-->/ =~ line
+        ns = $1.strip
+        it = $2.strip
+        (things[ns] ||= {})[it] ||= "0.00"
+        things[ns][it] = "%.02f" % (things[ns][it].to_r + $3.strip.to_r)
+      end
+    end
+  end
+  File.open filename.sub('.mkdn','.yaml'), 'w' do |file|
+    file.write(YAML::dump(things))
+  end
+end
+
