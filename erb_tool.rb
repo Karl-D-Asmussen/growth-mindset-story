44be8262be3e8c20bdeb0f272672b2162cc88bf6
Karl D. Asmussen <karl.asmussen@cpr.ku.dk>
2018-04-04 23:41:40 +0200

    Wed Apr  4 23:41:40 CEST 2018

---
+++ erb_tool.rb
@@ -0,0 +1,120 @@
+#! /usr/bin/ruby2.4
+
+END { _run(ARGV[0]) if $0 == __FILE__ }
+
+require 'yaml'
+require 'erb'
+require 'pathname'
+
+$DATA = {}
+
+
+def import(filename)
+  f = Pathname.new(filename)
+  y = f.sub_ext('.yaml')
+  if f.file? and f.readable?
+    if y.file? and y.readable? and y.mtime > f.mtime
+      $DATA = YAML.load(y.read())
+    else
+      _run(f.to_s) 
+    end
+  else
+    raise IOError, "cannot open file #{f} for reading" unless y.readable? and y.file?
+  end
+end
+
+def with(*dig)
+  dig = dig.flatten
+  raise TypeError, "#{dig.find {|x| not x.is_a?(String)}} is not a string" unless
+    dig.all? {|s| s.is_a?(String)} and not dig.empty?
+
+  at = $DATA
+  dig[0...-1].each do |ix|
+    at = (at[ix] ||= {})
+  end
+  at[dig.last] = yield at[dig.last]
+end
+
+def stat(*dig, at: nil)
+  raise TypeError, "#{at} is neither nil nor Float" unless
+    at.nil? or at.is_a?(Float)
+  
+  with(*dig) do |it|
+    if it.nil? or at
+      (at || 0.00).round(2)
+    elsif at.nil? and it.is_a?(Float)
+      it 
+    else
+      raise TypeError, "#{dig.fatten.join(':')} is not a stat"
+    end
+  end
+end
+
+def bump(*dig, by: 0.01)
+  raise TypeError, "#{by} is not a Float" unless
+    by.is_a?(Float)
+  
+  stat(*dig)
+
+  with(*dig) do |it|
+    (it + by).round(2)
+  end
+end
+
+def list(*dig, this: nil)
+  raise TypeError, "#{this} is neither nil nor String nor Array of Strings" unless
+    this.nil? or this.is_a?(String) or (this.is_a?(Array) and this.all? { |s| s.is_a?(String) })
+
+  this = [this] if this.is_a?(String)
+  
+  with(*dig) do |it|
+    if it.nil? or this
+      this || []
+    elsif to.nil? and it.is_a?(Array) and it.all? { |s| s.is_a?(String) }
+      it
+    else
+      raise TypeError, "#{dig.flatten.join(':')} is not a stat"
+    end
+  end
+end
+
+def add(*dig, this:)
+  raise TypeError, "#{this} is neither String nor Array of Strings" unless
+    this.is_a?(String) or (this.is_a?(Array) and this.all? { |s| s.is_a?(String) })
+  
+  this = [this] if this.is_a?(String)
+
+  list(*dig)
+
+  with(*dig) do |it|
+    it.concat(this)
+  end
+end
+
+def _capture
+  data = $DATA
+  $DATA = {}
+  yield
+  return $DATA
+ensure
+  $DATA = data
+end
+
+def _run(filename)
+  f = Pathname.new(filename)
+  if f.readable? and f.file?
+    ERB.new(f.read(), nil, '<> > -').result
+    _export(f.to_s)
+  else
+    raise IOError, "cannot open file #{f} for reading" unless y.readable? and y.file?
+  end
+  return nil
+end
+
+def _export(filename)
+  f = Pathname.new(filename)
+  y = f.sub_ext('.yaml')
+  raise IOError, "cannot open file #{y} for writing" unless y.writable? or not y.exist?
+  y.write(YAML.dump($DATA))
+  return nil
+end
---
+++ roll_2.rb
@@ -0,0 +1,77 @@
+#! /usr/bin/ruby2.4
+
+require './erb_tool'
+require 'distribution'
+
+END { main(ARGV) if $0 == __FILE__ }
+
+def main(argv)
+  import(argv[0])
+  things = what_things
+  puts show(things).join(' ')
+  target = values(things).sum.round(2)
+  success, roll, margin = roll(target)
+  puts '%.02f vs %.02f' % [target, roll]
+  puts '%s %+.02f' % [success ? 'success' : 'failure', margin]
+end
+
+def result(target, success, roll, margin)
+  ['%.02f vs %.02f' % [target, roll],
+   '%s %+.02f' % [success ? 'success' : 'failure', margin]].freeze
+end
+
+def roll(target)
+  r = Random.rand while (r ||= 1.0) == 1.0
+  r = Distribution::LogNormal::GSL_::p_value(r, Math.log(10), 1).round(2)
+  [ r <= target, r, target - r ].freeze
+end
+
+def what_things
+  $stdin.each_line.collect do |line|
+    line = line.chomp
+    if /^\s*[+-]\s*\d+.\d\d$/ =~ line
+      line.gsub(/\s+/,'').to_f
+    elsif /^\w+(?:\s+\w+)$/ =~ line
+      line.split
+    else
+      raise SyntaxError, "#{line}"
+    end
+  end.sort_by do |a, b|
+    case a
+    when Float
+      case b
+      when Float then 0
+      when Array then -1
+      end
+    when Array
+      case b
+      when Float then 1
+      when Array then 0
+      end
+    end
+  end
+end
+
+def show(things)
+  things.collect do |thing|
+    case thing
+    when Array
+      thing.join(':')
+    when Float
+      "%+.02f" % thing
+    end
+  end
+end
+
+def values(things)
+  things.collect do |thing|
+    case thing
+    when Float
+      thing
+    when Array
+      val = $DATA.dig(*thing)
+      raise TypeError, "#{thing.join(':')} is not a stat" unless val.is_a?(Float)
+      val
+    end
+  end
+end
