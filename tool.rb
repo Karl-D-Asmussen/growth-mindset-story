c73b113e5ca8be14ea7cc131ce9fa0ca444ca157
Karl D. Asmussen <karl.asmussen@cpr.ku.dk>
2018-04-04 12:58:19 +0200

    Wed Apr  4 12:58:19 CEST 2018

--- tool.rb
+++ tool.rb
@@ -7,34 +7,55 @@ things = Hash.new { |h,k| h[k] = {} }
 
 filenames.each do |filename|
   File.open filename do |file|
-    file.each_line do |line|
+    file.each_line.each_with_index do |line, linenum|
+      linenum += 1
       if /<!--(.+)::(.+)=(\s*\d+\.\d\d\s*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
+        if things.dig(ns, it).is_a? Array
+          $stderr.puts "#{filename}:#{linenum} type overwrite"
+        end
         things[ns][it] = $3.strip
-      if /<!--(.+)::(.+)\+=(\s*\d+\.\d\d\s*)-->/ =~ line
+      elsif /<!--(.+)::(.+)\+=(\s*\d+\.\d\d\s*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        things[ns][it] ||= '0.00'
+        if things.dig(ns, it).is_a? Array
+          $stderr.puts "#{filename}:#{linenum} type error"
+          things[ns][it] = '0.00'
+        end
         things[ns][it] = '%.02f' % (things[ns][it].to_r + $3.strip.to_r)
       elsif /<!--(.+)::(.+)@(.*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        things[ns][it] = ($3.empty? ? [] : [$3.strip])
+        if things.dig(ns, it).is_a? String
+          $stderr.puts "#{filename}:#{linenum} type overwrite"
+        end
+        things[ns][it] = ($3.strip.empty? ? [] : [$3.strip])
       elsif /<!--(.+)::(.+)\+@(.*)-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        things[ns][it] ||= []
+        if things.dig(ns, it).is_a? String
+          $stderr.puts "#{filename}:#{linenum} type error"
+          things[ns][it] = []
+        end
         things[ns][it] << $3.strip
       elsif /<!--(.+)::(.+)!\s*-->/ =~ line
         ns = $1.split.join
         it = $2.split.join
-        things[ns].delete(it)
-        if things[ns].empty?
-          things.delete(ns)
+        if things.dig(ns, it)
+          things[ns].delete(it)
+          if things[ns].empty?
+            things.delete(ns)
+          end
+        else
+          $stderr.puts "#{filename}:#{linenum} not found"
         end
+      elsif /<!--(.+)::(.+)\?\s*-->/ =~ line
+        ns = $1.split.join
+        it = $2.split.join
+        $stderr.puts "#{filename}:#{linenum} #{ns}::#{it} := #{things.dig(ns, it).inspect}"
       elsif /<!--.*-->/ =~ line
-        $stderr.puts "error #$&"
+        $stderr.puts "#{filename}:#{linenum} syntax error"
       end
     end
   end
